7f583db9badda14dc189b911cc044f1fa628cdd9
Jesse <jesse@blinq.me>
2024-09-03 14:06:11 +1000

    Add custom keymappings to easily access NvimTree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,3 +1,19 @@
+-- NvimTree Keymappings
+local function tree_on_attach(bufnr)
+  local tree_api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default
+  tree_api.config.mappings.default_on_attach(bufnr)
+
+  -- custom
+  vim.keymap.set('n', '<leader>tt', tree_api.tree.toggle, {})
+  vim.keymap.set('n', '<leader>tf', tree_api.tree.find_file, {})
+end
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -6,7 +22,8 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      on_attach = tree_on_attach,
+    }
   end,
 }
-
