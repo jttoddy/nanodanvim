8c2d738e498a1b70b6f37fc1512092976cc9144c
Jesse <jesse@blinq.me>
2024-09-11 12:08:13 +1000

    Add ruby filetype syntax associations for Fastlane resource files

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@ vim.wo.relativenumber = true
 
 vim.cmd("colorscheme kanagawa")
 
+-- Fastlane associations
+vim.cmd[[au BufNewFile,BufRead Fastfile set syntax=ruby]]
+vim.cmd[[au BufNewFile,BufRead Appfile set syntax=ruby]]
+vim.cmd[[au BufNewFile,BufRead Gymfile set syntax=ruby]]
+vim.cmd[[au BufNewFile,BufRead Matchfile set syntax=ruby]]
+vim.cmd[[au BufNewFile,BufRead Pluginfile set syntax=ruby]]
+vim.cmd[[au BufNewFile,BufRead Scanfile set syntax=ruby]]
+vim.cmd[[au BufNewFile,BufRead Deliverfile set syntax=ruby]]
